05e6d828322fea7ac9d5f65014d410ee4f30948e
AaronGlen <Aaron.Glendinning@academytrainee.com>
2019-08-20 14:07:33 +0100

    initial commit

---
+++ src/main/java/com/qa/App.java
@@ -0,0 +1,17 @@
+package com.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = 2;
+        int b = 4;
+
+        System.out.print(a);
+
+    }
+}
