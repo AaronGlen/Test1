f5354548b2b2ddc9f64a84c3a94a5b0cbbbb6f03
AaronGlen <Aaron.Glendinning@academytrainee.com>
2019-08-20 15:17:40 +0100

    first working test

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -1,23 +1,38 @@
 package com.qa;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
+
 {
+    private App app;
+
+    @Before
+    public void initialise(){
+        app = new App();
+    }
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void test1(){
-        fail("fail");
+    public void mainTest(){
+        app = new App();
+        String[] input = new String[]{"a"};
+        app.main(input);
     }
-    @test
-    public void test1(){
-        fail("fail");
+
+    @Test
+    public void addNumber(){
+        
+        assertEquals("returning?", 2, app.input2(2,5));
     }
 }
